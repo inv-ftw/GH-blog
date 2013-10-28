8048d1cb1a25e215a53be37843f2e8f832bc9303
inv-ftw <inv.ftw@gmail.com>
2013-10-28 10:23:30 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = '12f4d8267d2527e0ff0f83d78e93980ebb93e60726ed3a2be135d580ddb850f8977bd5e49b7d01a2a78e4ce207df4d40f0b9df00c7fb31bc423d3c19a0a26547'
