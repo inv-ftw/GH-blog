44d273371de3097c4cdd8e5ec87415035fbe989e
inv-ftw <inv.ftw@gmail.com>
2013-11-05 22:05:43 +0200

    registration and auth

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131028081856) do
+ActiveRecord::Schema.define(version: 20131029171003) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
     t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
